44ab2eab1a79177d9a8683e1fb4c9c4c52ce1940
DESKTOP-O99GIDU\smoli <smolijartem@gmail.com>
2026-03-13 18:44:41 +0100

    DONE LAB_3

--- Django/mysite/mysite/urls.py
+++ Django/mysite/mysite/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
 
+admin.site.site_header = "Polls Administration"
+admin.site.site_title = "Polls Admin Portal"
+admin.site.index_title = "Welcome to Polls Researcher Portal"
+
 urlpatterns = [
-    path("polls/", include("polls.urls")),
+    path("polls/", include("polls.urls")), # Має бути саме polls.urls
     path("admin/", admin.site.urls),
 ]
