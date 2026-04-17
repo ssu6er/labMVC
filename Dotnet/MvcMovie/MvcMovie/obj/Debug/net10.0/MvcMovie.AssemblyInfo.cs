413a44d057557e733907c5165d23cb986e23afdb
DESKTOP-O99GIDU\smoli <smolijartem@gmail.com>
2026-04-17 15:01:25 +0200

    Done LAB_6

--- Dotnet/MvcMovie/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
+++ Dotnet/MvcMovie/MvcMovie/obj/Debug/net10.0/MvcMovie.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e58d499c7907efafb8eb933d281a3209471c4a43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6a3a200cf1ffdcc129d00fd36f2f263d5b34ae7")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
